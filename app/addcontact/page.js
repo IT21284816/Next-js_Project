2c11bdbf7bb1c8daa39518902cff3fabf850d238
Duhun De Silva <sandaruduhun@gmail.com>
2024-04-13 18:56:54 +0530

    initial commit

---
+++ app/addcontact/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react'
+import AddContact from './AddContact'
+
+export default function page() {
+  return (
+    <div>
+        <h1>add contact</h1>
+        <AddContact/>
+    </div>
+  )
+}
