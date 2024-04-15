14b2e2af37d2d6279cbcd4f5ee4b0c48df5724f8
Duhun De Silva <sandaruduhun@gmail.com>
2024-04-15 14:30:12 +0530

    initial commit

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Image from 'next/image';
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
@@ -34,34 +35,58 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="grid place-items-center h-screen">
-      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
-        <h1 className="text-xl font-bold my-4">Login</h1>
+    <div className="flex">
+      <div className="relative overflow-hidden w-3/5 min-h-screen" style={{ backgroundImage: 'url("/image.png")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <div className="absolute inset-0 bg-custom-color" style={{ clipPath: 'circle(100% at 0 50%)' }}></div>
+        <div className="relative z-10">
+          {/* Content for the left half */}
+          
+          <div className="grid h-screen justify-start items-center ml-[10%]">
+      <div className=" text-white mb-5" >
+        <h1 className="text-4xl font-bold my-4 ">Hi there,</h1>
 
-        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-          <input
+        <div className="text-2xl font-medium mb-16">
+        <p> Welcome to our</p>
+        <p> contacts portal</p>
+        </div>
+
+        
+        <form onSubmit={handleSubmit} className="flex flex-col gap-6 ">
+          <input className="rounded-[20px] bg-white placeholder-[#083F46] font-semibold"
             onChange={(e) => setEmail(e.target.value)}
             type="text"
-            placeholder="Email"
+            placeholder="e-mail"
           />
-          <input
+          <input className="rounded-[20px] bg-white placeholder-[#083F46] font-semibold"
             onChange={(e) => setPassword(e.target.value)}
             type="password"
-            placeholder="Password"
+            placeholder="password"
           />
-          <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
-            Login
+
+          <div className ="flex mt-6">
+          <button className="bg-[#083F46] rounded-[20px] w-[100px] h-[40px] text-white font-semibold cursor-pointer px-6 py-2  border border-white">
+            login
           </button>
+          <Link className="text-sm mt-3 text-right text-[18px] ml-4" href={"/register"}>
+            or <span className="underline">Click here to Register</span>
+          </Link>
+          </div>
+         
           {error && (
-            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-5">
               {error}
             </div>
           )}
 
-          <Link className="text-sm mt-3 text-right" href={"/register"}>
-            Don't have an account? <span className="underline">Register</span>
-          </Link>
+          
         </form>
+      </div>
+    </div>
+
+        </div>
+      </div>
+      <div className="w-2/5 min-h-screen" style={{ backgroundImage: 'url("/image.png")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        {/* Content for the right half */}
       </div>
     </div>
   );
